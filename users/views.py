1cfa82946579db05c4553978f10e209dc2f03fd6
koresachin7 <koresachin7@gmail.com>
2023-06-17 14:34:42 +0530

    Add:- User Registration API for registr user

--- users/views.py
+++ users/views.py
@@ -4,4 +4,32 @@
 * @Title: To Do list user registration
 """
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from users.models import User
+
+
+@csrf_exempt
+def user_registration(request):
+    """
+    Description:
+        This Function is creating for user registrations
+    :param request:
+    :return: JsonResponse
+    """
+    try:
+        if request.method == "POST":
+
+            data = json.loads(request.body)
+            user_name = data.get("user_name")
+
+            if User.objects.filter(user_name=user_name).exists():
+                return JsonResponse({"message": "Username is already Take", "data": {"username": user_name}})
+            else:
+                user = User(user_name=user_name, first_name=data.get("first_name"), last_name=data.get("last_name"),
+                            password=data.get("password"))
+                user.save()
+                return JsonResponse({"message": "User data Successfully Sava", "data": data})
+
+    except Exception as e:
+        return HttpResponse("Data is invalid", e)
